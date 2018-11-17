5a0d26352394759d03eda75b061fd5a31ff83500
Polskaya Marina <marina_0880@mail.ru>
2018-11-17 17:57:18 +0700

    Добавлен файл Header.php  с объявлением класса.

--- form-handler.php
+++ form-handler.php
@@ -1,13 +1,22 @@
 <?php
-header('Location: /lesson17.11/index.php');
+// header('Location: /lesson17.11/index.php');
+// echo "<pre>";
+// print_r([
+// 	'request' => $_REQUEST,
+// 	'post' => $_POST,
+// 	'get' => $_GET,
+// 	'globals' => $GLOBALS
+// ]);
+// echo "</pre>";
+require_once 'Handler.php';
+
+$handler = new Handler();
+
 echo "<pre>";
-print_r([
-	'request' => $_REQUEST,
-	'post' => $_POST,
-	'get' => $_GET,
-	'globals' => $GLOBALS
+print_r ([
+	$_POST,
+	$handler->execute($_POST)
 ]);
 echo "</pre>";
-
 
 ?>
